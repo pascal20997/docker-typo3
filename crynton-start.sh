aaff58ef322b8e7d040ac2535dda2a8fed133cdd
Pascal <info@crynton.com>
2018-06-10 02:12:19 +0200

    [BUGFIX] Run composer as user crynton

--- crynton-start.sh
+++ crynton-start.sh
@@ -11,14 +11,14 @@
 # install TYPO3 if INSTALL_TYPO3 = true
 if [ "${INSTALL_TYPO3}" = "true" ]; then
     echo "Check if composer.json exists..."
-    if [ -f "${DOCUMENT_ROOT}/composer.json" ]; then
+    if [ -f "${DOCUMENT_ROOT}/../composer.json" ]; then
         echo "The file composer.json exists! Skip TYPO3 installation..."
     else
         echo "The file composer.json does not exists! Install TYPO3 ${TYPO3_VERSION}..."
-        composer create-project typo3/cms-base-distribution typo3 ${TYPO3_VERSION}
-        if [ $? -ne 0 ]; then
-            echo "Something went wrong while installing TYPO3 :( Please check the composer output above!"
-            exit 1
+        su -l crynton -c "composer create-project typo3/cms-base-distribution typo3 ${TYPO3_VERSION}"
+#        if [ $? -ne 0 ]; then
+#            echo "Something went wrong while installing TYPO3 :( Please check the composer output above!"
+#            exit 1
         fi
     fi
 else
